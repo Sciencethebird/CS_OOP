a4c00221ba809aee8f9cd0fe146ed6a251a2d61c
sciencethebird <sciencethebird@gmail.com>
2018-10-19 01:06:12 +0800

    re-adding final

---
+++ Final/Code Correction/FInal-3/FInal-3/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  FInal-3
+//
+//  Created by Sciencethebird on 2018/8/28.
+//  Copyright © 2018年 Sciencethebird. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ Final/Code Correction/Final-2/Final-2/ZipCode .h
@@ -0,0 +1,24 @@
+#ifndef ZIPCODE_H
+#define ZIPCODE_H
+
+const int ZipSize = 3;
+const char InitChar = '?';
+const char Hyphen = '-';
+const int SuffixLen = 2;
+
+class ZipCode
+{
+    public:
+        ZipCode();
+        ZipCode(int);
+        ZipCode(const char*);
+        ZipCode(ZipCode&);
+        ~ZipCode();
+        void extend(int);
+        void extend(const char*);
+        friend std::ostream &operator << (std::ostream &out, const ZipCode&);
+    private:
+        char *code;
+};
+
+#endif
